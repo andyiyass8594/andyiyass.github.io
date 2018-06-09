2aefbf289817ec54af753711ad9c3a073e31fdd7
andyiyass8594 <40127930+andyiyass8594@users.noreply.github.com>
2018-06-09 14:50:10 -0700

    my upload site

---
+++ app/config/config.php
@@ -0,0 +1,65 @@
+<?php
+$oc_config = array (
+    
+    //================================================================================================
+   
+
+    "favicon"         => "favicon.gif" ,
+
+    //================================================================================================
+
+    "sitelogo"        => "KodiTV.site",
+    
+    "sitename"        => "Movies & TVshow",
+
+    "sitedescription" => "Watch Full Movies Streaming HD Quality and Full Episode TV Shows for FREE",
+
+    "sitekeywords"    => "full movie, full episode",
+
+    "sitethemes"      => "muvimag_v3",
+
+    "offer_link1"     => "//look.kfiopkln.com/offer?prod=3&ref=5139824",
+
+    "offer_link2"     => "//look.kfiopkln.com/offer?prod=3&ref=5139824",
+
+    // "sport_offer"     => "",        // Jika Belum Di Include Plugin Sport nya gak bisa di gunakan
+
+    "histatsID"       => "4074648",
+
+    //================================================================================================
+
+    "tvdb_search"     => "true",
+
+    "cache"           => "true",
+
+    "_seo"            => "true",
+
+    "_stt"            => "true",
+
+    "protect_content" => "false",       // disable klik kanan, block text, copy paste pada halaman LP
+
+    "search_url"      => "search",
+
+    "url_end"         => ".html",
+    
+    "category_url"    => "genre",
+    
+    "timezone"        => "Asia/Jakarta",
+
+    "debug"           => "true",
+
+);
+
+$alt_country2 = array(
+    /*
+    |--------------------------------------------------------------------------
+    | Contoh Penulisan Country Code
+    |--------------------------------------------------------------------------
+    | 'US', 'GB', 'UK'
+    | Contoh Dibawah Menandakan Negara ID (Indonesia) dan MY (Malaysia) 
+    | Offer Link nya akan di redirect ke Offer Kedua
+    |
+    */
+   
+   'ID', 'MY', 'MX', 'RU'
+);
